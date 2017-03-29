d5d98606ede779370df64cfc3f5d2e53ab238847
Piehamster <pie@piehamster.com>
2017-03-29 12:37:19 +0100

    Models

--- pieboard/imgboard/models.py
+++ pieboard/imgboard/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    md5 = models.CharField(max_length=32)
+    rating = models.CharField(max_length=12)
+    uploaded_on = models.DateTimeField()
+    uploaded_by = models.IntegerField()
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=64)
+    type = models.CharField(max_length=16)
+
+
+class ImageTags(models.Model):
+    image_id = models.ForeignKey(Image)
+    tag_id = models.ForeignKey(Tag)
